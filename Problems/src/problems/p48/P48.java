1fa9bd25dc31a8046d5722917c48ef918c2f53dc
Kevin <Kevin@Mirzam>
2014-02-22 22:25:42 -0600

    Problem 48 - statement

---
+++ Problems/src/problems/p48/P48.java
@@ -0,0 +1,13 @@
+package problems.p48;
+
+public class P48 {
+	/*
+	 * The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+	 * 
+	 * Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
