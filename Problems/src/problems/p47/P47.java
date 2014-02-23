d452e3a4d1c97972491ab7652704b47036a7e58d
Kevin <Kevin@Mirzam>
2014-02-22 22:23:37 -0600

    Problem 47 - solved!

--- Problems/src/problems/p47/P47.java
+++ Problems/src/problems/p47/P47.java
@@ -1,5 +1,10 @@
 package problems.p47;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+import utils.PrimeUtils;
+
 public class P47 {
 	/*
 	 * The first two consecutive numbers to have two distinct prime factors are:
@@ -19,7 +24,39 @@ public class P47 {
 	 */
 	
 	public static void main(String[] args) {
-
+//		System.out.println(distinctFactorCount(14));
+//		System.out.println(distinctFactorCount(15));
+//		
+//		System.out.println(distinctFactorCount(644));
+//		System.out.println(distinctFactorCount(645));
+//		System.out.println(distinctFactorCount(646));
+		
+		long cap = 1000000;
+		long count = 4;
+		for(long n=1; n<cap; n+=count) {
+			if (distinctFactorCount(n) != count) continue;
+			
+			long l = n-1;
+			while(distinctFactorCount(l) == count) l--;
+			long start = l+1;
+			
+			l = n+1;
+			while(distinctFactorCount(l) == count) l++;
+			long end = l-1;
+			if ((end + 1 - start) >= count) {
+				System.out.println("Match: [" + start + ", " + end + "]");
+			}
+		}
+	}
+	
+	private static int distinctFactorCount(long n) {
+		long[] primeFactors = PrimeUtils.computePrimeFactors(n);
+		HashSet<Long> distinctFactors = new HashSet<>();
+		for(long primeFactor : primeFactors) {
+			distinctFactors.add(primeFactor);
+		}
+		
+		return distinctFactors.size();
 	}
 
 }
