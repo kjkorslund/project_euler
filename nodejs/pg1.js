3ea2254c3a8b1f09fcb60167c329b3ee0500c046
Kevin K <1168788+kjkorslund@users.noreply.github.com>
2025-08-20 16:15:11 -0400

    nodejs: P9 solution

--- nodejs/pg1.js
+++ nodejs/pg1.js
@@ -224,3 +224,22 @@ pg1.p8 = function() {
   console.log(match);
   return matchProduct;
 }
+
+/**
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *     a^2 + b^2 = c^2.
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ * 
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.  Find the product abc.
+ */
+pg1.p9 = function() {
+  for(let a=1; a<1000; a++) {
+    for (let b=a; b<1000; b++) {
+      let c = 1000 - (a + b);
+      if (a*a + b*b == c*c) {
+        console.log(`Found [${[a,b,c]}]`)
+        return a*b*c;
+      }
+    }
+  }
+}
