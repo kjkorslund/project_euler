64dab8e83d31a256ece48b272bd12618d1ed22cd
Kevin K <1168788+kjkorslund@users.noreply.github.com>
2025-08-24 16:00:30 -0400

    csharp: P9 solution

--- csharp/project_euler/Program.cs
+++ csharp/project_euler/Program.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 using Project_euler.Problems;
 
-var problem = new P8();
+var problem = new P9();
 var s = Stopwatch.StartNew();
 var answer = problem.Solve();
 s.Stop();
--- csharp/project_euler/problems/Page1.cs
+++ csharp/project_euler/problems/Page1.cs
@@ -252,3 +252,32 @@ class P8 : IProblem<long>
       .Select(it => (long)(it - '0'))
       .Aggregate((d1, d2) => d1 * d2);
 }
+
+/**
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *     a^2 + b^2 = c^2.
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ * 
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.  Find the product abc.
+ */
+class P9 : IProblem<int>
+{
+  public int Solve()
+  {
+    for (int a = 1; a < 1000; a++)
+    {
+      for (int b = a; b < 1000; b++)
+      {
+        int c = 1000 - (a + b);
+        if (c < b) break;
+        if (a * a + b * b == c * c)
+        {
+          int product = a * b * c;
+          Console.WriteLine($"{a}^2 + {b}^2 = {c}^2 = 1000  => {product}");
+          return product;
+        }
+      }
+    }
+    return -1;
+  }
+}
