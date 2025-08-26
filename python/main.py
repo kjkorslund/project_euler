87f66446bda2b4af5d965c565332186e1a19ef39
Kevin K <1168788+kjkorslund@users.noreply.github.com>
2025-08-25 20:55:20 -0400

    python: P2 solution

--- python/main.py
+++ python/main.py
@@ -2,14 +2,9 @@ import time
 from extensions import IntExt
 import problems.page1
 
-solver = problems.page1.P1
+solver = problems.page1.P2
 start_time = time.time()
 answer = solver();
 end_time = time.time()
 elapsed_ms = int((end_time - start_time) * 1000)
 print(f"Answer for {solver.__name__}: {answer} ({elapsed_ms} ms)")
-
-i = IntExt(2)
-print(f"{i}: {i.is_even()} / {i.is_odd()}")
-i = IntExt(3)
-print(f"{i}: {i.is_even()} / {i.is_odd()}")
