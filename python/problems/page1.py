fddcf0d3c853fa93b4f4e14c84a1fc305f265dd2
Kevin K <1168788+kjkorslund@users.noreply.github.com>
2025-08-28 23:12:34 -0400

    python: P9 solution

--- python/problems/page1.py
+++ python/problems/page1.py
@@ -169,3 +169,18 @@ def P8():
       result, resultProduct = s, product
   print(f"{result} => {resultProduct}")
   return resultProduct
+
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#     a^2 + b^2 = c^2.
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+# 
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.  Find the product abc.
+def P9():
+  for a in range(1,500):
+    for b in range(a,1000-a):
+      c = 1000 - (a + b)
+      if c < b: break
+      if a*a + b*b == c*c:
+        print(f"{a}^2 + {b}^2 == {c}^2 == {a*b*c}")
+        return a*b*c
+  return -1
